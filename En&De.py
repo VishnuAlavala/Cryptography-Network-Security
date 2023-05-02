249af386df44322e6167ff32de1021e5b3c23941
Known_as_VisHnu <114065646+VishnuAlavala@users.noreply.github.com>
2023-05-02 08:50:57 +0530

    Create En&De.py

---
+++ En&De.py
@@ -0,0 +1,18 @@
+import string
+a=input("Enter Plain Text:")
+b=int(input("Enter Key:"))
+plain_text=a
+shift=b
+alphabet=string.ascii_lowercase
+shifted=alphabet[shift:] + alphabet[:shift]
+table=str.maketrans(alphabet,shifted)
+encrypted=plain_text.translate(table)
+print("Cipher Text:",encrypted)
+c=encrypted
+shift=-b
+alphabet=string.ascii_lowercase
+shifted=alphabet[shift:] + alphabet[:shift]
+table=str.maketrans(alphabet,shifted)
+encrypted=c.translate(table)
+print("Plain Text:",encrypted)
+
